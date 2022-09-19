86f20cc8273b562c9b6e36374f15d209be56e23f
Carlos Villanueva <cvillanueva@myafo.com>
2022-09-19 13:47:54 -0500

    initial commit - set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+  ]),
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  styles: [`
+
+  `],
+  template: `
+    test
+  `
+})
+
+export class HomeComponent implements OnInit {
+
+
+  constructor(
+
+  ) {}
+
+  ngOnInit() {
+
+  }
+
+
+}
